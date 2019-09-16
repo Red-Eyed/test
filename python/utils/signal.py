26b3d0e666e11a9ab34bc43923a41befb291dcc3
Vadym Stupakov <vadim.stupakov@gmail.com>
2019-09-16 08:04:37 +0300

    - Removed np.sign and added np.where

--- python/utils/signal.py
+++ python/utils/signal.py
@@ -3,11 +3,15 @@ from scipy.signal import convolve
 
 
 def find_peaks1d(array: np.ndarray):
-    derivative = convolve(array, [1.0, -1.0], "same")
+    kernel = np.float32([1, -1])
+    darray = convolve(array, kernel, "same")
 
-    sign = np.sign(derivative)
-    sign_change = convolve(sign, [1.0, -1.0], "same")
-    sign_change[[0, -1]] = 0
-    peaks = sign_change == sign_change.min()
+    sign = np.where(darray <= 0, -1, 1)
+    dsign = convolve(sign, kernel, "same")
+
+    # removing zero padding
+    dsign[[0, -1]] = 0
+
+    peaks = dsign == dsign.min()
 
     return np.uint32(peaks)
